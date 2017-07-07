da18b65d9300908d920b705cf4cb67de570a5cce
smpx <rajatsri94@gmail.com>
2017-07-07 17:43:37 +0530

    geolocations added

--- migrations/20170706163632_ninjas.js
+++ migrations/20170706163632_ninjas.js
@@ -5,11 +5,19 @@ exports.up = function(knex, Promise) {
 
 	return Promise.all([
 
-		knex.schema.createTable('ninjas', function(table) {
+		knex.schema.createTableIfNotExists('geo', function(table) {
 			table.increments('id').primary()
-			table.string('name').notNullable()
+			table.string('type').defaultTo("Point")
+			table.enu('coordinates', ['lat', 'lon'])
+		})
+		.createTableIfNotExists('ninjas', function(table) {
+			table.increments('id').primary()
+			table.string('name').notNullable().unique()
 			table.string('rank').notNullable()
 			table.boolean('available').defaultTo(false)
+			// table.foreign('geometry').references('geo.id')
+			table.double('lat')
+			table.double('lon')
 		})
 	])
 };
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,38 +1,86 @@
 import Koa from 'koa'
-import Route from 'koa-router'
+import Router from 'koa-router'
 import knex from '../db'
 
-const app = new Koa()
-const route = new Route({
-		prefix: '/api'
-})
+const app = new Koa();
+const router = new Router({
+	prefix: '/api'
+});
+
+let distance = (lat1, lon1, lat2, lon2) => {
+	const R = 6371e3; // metres
+	const F = 22/(7*180)
+	console.log(lat1, lat2)
+	let φ1 = lat1*F;
+	let φ2 = lat2*F;
+	let Δφ = (lat2-lat1)*F;
+	let Δλ = (lon2-lon1)*F;
+
+	let a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+	        Math.cos(φ1) * Math.cos(φ2) *
+	        Math.sin(Δλ/2) * Math.sin(Δλ/2);
+	let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+	let d = Math.round(R * c / 1000, 2);
+	return d
+}
 
 // get a list of ninjas from database
-route.get('/ninjas', ctx => {
-	ctx.body = {type: 'GET'}
+router.get('/ninjas', async(ctx, next) => {
+	const result = await knex('ninjas').select()
+	const lat = ctx.request.query.lat
+	const lon = ctx.request.query.lon
+	result.forEach( ninja => {
+		let dist = distance(lat, lon, ninja.lat, ninja.lon)
+		console.log(ninja.name + " is "+ dist +" km away")
+	})
 })
 
 // add a new ninja to the database
-route.post('/ninjas', async (ctx, next) => {
+router.post('/ninjas', async (ctx, next) => {
 	const ninja = ctx.request.body
-	await knex('ninjas')
-		.insert(ninja)
-		.then((ninja) => {
-			ctx.body = "data successfully inserted"
-		})
-		.catch(function(err) {
-			ctx.body = err.message
-		})
+	try {
+		const id = await knex('ninjas').insert(ninja)
+		const result = await knex('ninjas').where('id', id)
+		console.log(result)
+		ctx.body = result
+	} catch (err) {
+		console.error(err);
+	}
 })
 
 // update ninja in database
-route.put('/ninjas/:id', (ctx) => {
-	ctx.body = {type: 'PUT'}
+router.put('/ninjas/:id', async(ctx, next) => {
+	const id = ctx.params.id
+	const newData = ctx.request.body
+	try {
+		const update = await knex('ninjas').where('id', id).update(newData)
+		const result = await knex('ninjas').where('id', id)
+		console.log(result)
+		ctx.body = result
+	} catch (err) {
+		console.error(err)
+		ctx.body = err
+	}
 })
 
 // delete ninja from database
-route.delete('/ninjas/:id', (ctx) => {
-	ctx.body = {type: 'DELETE'}
+router.delete('/ninjas/:id', async(ctx, next) => {
+	const id = ctx.params.id
+	try {
+		const result = await knex('ninjas').where('id', id).del()
+		console.log(result)
+		ctx.body = "successfully deleted"
+	} catch (err) {
+		console.error(err)
+		ctx.body = err
+	}
 })
 
-export default route
+// for any other requests
+router.get('/(.*)', async(ctx,next) => {
+	console.log(ctx.params);
+	Object.keys(ctx.params).map(key => console.log(ctx.params[key]));
+})
+
+export default router
